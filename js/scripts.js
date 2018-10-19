fe302cf3e448baa14f232f59811fd1b0a1b843dd
Christopher Cahill and leo mangutov <christopher@>
2018-10-18 17:19:12 -0700

    adds some drawing stuff and cleans up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-// function draw() {
-//   var canvas = document.getElementById('canvas');
-//   if (canvas.getContext) {
-//     var ctx = canvas.getContext('2d');
-//
-//     ctx.beginPath();
-//     ctx.moveTo(75, 50);
-//     ctx.lineTo(100, 75);
-//     ctx.lineTo(100, 25);
-//     ctx.fill();
-//   }
-// }
-
 
 $(document).ready(function() {
   $("form#triangleNumbers").submit(function(event) {
@@ -34,33 +21,13 @@ $(document).ready(function() {
     }
 
 
-    // function draw() {
-//
-//
-// var canvas = document.getElementById('triangle-canvas');
-// var ctx = canvas.getContext('2d');
-//
-// var R1=120, R2=140, R3=90;
-// var Ax=0, Ay=0;
-// var Bx=R3, By=0;
-// var Cx=(R2*R1+R3*R3-R1*R1)/(2*R3);
-// var Cy=Math.sqrt(R2*R2-Cx*Cx);
-//
-// var Ox = canvas.width / 2 - Bx/2;
-// var Oy = canvas.height / 2 + Cy/2;
-//
-//
-// ctx.beginPath();
-// ctx.moveTo(Ox+Ax, Oy-Ay);
-// ctx.lineTo(Ox+Bx, Oy-By);
-// ctx.lineTo(Ox+Cx, Oy-Cy);
-// ctx.closePath();
-// ctx.fillStyle="gold"; ctx.lineWidth=2;
-// ctx.stroke(); ctx.fill();
-//
-// }
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
 
-  event.preventDefault();
+event.preventDefault();
 
   });
 
